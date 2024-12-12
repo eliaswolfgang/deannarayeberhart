ae1e13907bd20ecaa6694308a9056014b4e6d47e
Ian Von Fange <ivonfange@instructors.2u.com>
2024-12-11 19:39:15 -0600

    Another fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,11 @@
-import { ReactElement } from 'react';
 import coverImage from '../../assets/cover/cover-image.jpg';
 
-const Header: React.FC<{children: ReactElement[]}> = ({children}) => {
+const Header: React.FC = () => {
 
   return (
     <header className="flex-row space-between px-1">
       <h1>Deanna Ray Eberhart</h1>
       <img src={coverImage} alt="wooden background"></img>
-      {children}
     </header>
   );
 }
