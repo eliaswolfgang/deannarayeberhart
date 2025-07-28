96a1b04db579a14172e2e10d8f4ea1bb4d15ce0a
Ian Von Fange <ivonfange@instructors.2u.com>
2025-07-28 03:29:35 -0500

    trying to fix resume

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -3,7 +3,7 @@ import Resume_PDF from '../../assets/Resume_2025.pdf';
 function Resume() {
   return (
     <div className='d-flex flex-column justify-content-center mt-3'>
-      <object style={{ height: '100vh', width: '100%' }} data={Resume_PDF}></object>
+      <object style={{ height: '100vh' }} data={Resume_PDF}></object>
     </div>
   );
 }
